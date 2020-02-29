c1611864753f3138d41fe605b878edd4ca422019
vaie <vaielx@163.com>
2020-02-29 15:50:54 +0800

    Initial commit

---
+++ src/main/java/com/outdd/awmcustomerserver/AwmCustomerServerApplication.java
@@ -0,0 +1,13 @@
+package com.outdd.awmcustomerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwmCustomerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwmCustomerServerApplication.class, args);
+    }
+
+}
