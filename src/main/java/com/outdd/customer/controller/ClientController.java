f12f8fbe165ca163dc640d6d0533ba0f659e33b5
oldStyle1102 <hypisces1125@sina.com>
2020-04-20 01:12:50 +0800

    1.customer模块修改 2.iml文件更新

--- src/main/java/com/outdd/customer/controller/ClientController.java
+++ src/main/java/com/outdd/customer/controller/ClientController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author vaie 1
+ * @author vaie
  * @Created 2019/1/28 15:20
  */
 @RestController
